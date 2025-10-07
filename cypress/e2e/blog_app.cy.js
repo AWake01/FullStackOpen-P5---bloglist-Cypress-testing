cd35956b16b71910f2f0643173d60ee7a25a8fe8
AWake01 <40424954+AWake01@users.noreply.github.com>
2025-10-07 16:24:22 +0100

    Exercise 5.17 complete:  added test for page open

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function() {
+      cy.contains('Log in to application')
   })
 })
